f1d81af1cb6d60ce95e7d999d1434f8db8d92141
bkroggel <b.kroggel@yahoo.de>
2015-07-16 11:40:36 +0200

    css strucktur angepasst, inhaltsverzeichnis, kleinere modifikationen an der html strucktur, scrolleffekt in javascript, clickbares logo in der navbar

--- js/index.js
+++ js/index.js
@@ -65,6 +65,17 @@ $(document).ready(function(){
       slideout.close();
     });
 
+    //scroll effect on navigation bar (not mobile)
+    var $root = $('html, body');
+        $('.nav a').click(function() {
+            var href = $.attr(this, 'href');
+            $root.animate({
+                scrollTop: $(href).offset().top
+            }, 500, function () {
+                window.location.hash = href;
+            });
+            return false;
+        });
 
   // end document ready
 });
